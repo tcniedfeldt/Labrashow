a85c3989de14daf53d77bf060e6498e6204b7c0a
Thomas Niedfeldt <30270676+tcniedfeldt@users.noreply.github.com>
2018-02-15 20:59:14 -0700

    tested json

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ $(document).ready(function() {
                         success : function(json) {
                                 console.log(json);
 
-                                var results = "";
+                                //var results = "";
                                 //results += '<h2>Answer to Query: ' + value + '</h2>';
                                 //for (var i=0; i < json.items.length; i++) {
                                 //}
-                                $("#results").html(results);
+                                $("#results").html(json);
                         }
                 });
         });
