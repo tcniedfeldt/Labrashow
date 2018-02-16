d92d8efbf57d2075ccb454be74576c2f752f3315
Thomas Niedfeldt <30270676+tcniedfeldt@users.noreply.github.com>
2018-02-15 21:40:46 -0700

    rearranged results

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
                 e.preventDefault();
 		console.log("Roulette Begun!\n");
 
-		var movie = Math.floor(Math.random() * 1000) + 2;
+		var movie = Math.floor(Math.random() * 500000) + 2;
                 var myurl="https://api.themoviedb.org/3/movie/" + movie + "?api_key=c59173680a889c0419dae6d8320bd867";
                 $.ajax({
                         url : myurl,
@@ -15,8 +15,18 @@ $(document).ready(function() {
 				results += '<img src="https://image.tmdb.org/t/p/w154' + json.poster_path + '"/>';
 				results += '<h3>' + json.title + '</h3>';
 				results += '<p>' + json.tagline + '</p>';
-				//overview
-				//rating
+
+				//Genres
+				results += '<p>Genre(s): ';
+				for (var i = 0; i < json.genres.length; i++) {
+					if (i === 0) {
+						results += json.genres[i].name;
+					} else {
+						results += ', ' + json.genres[i].name;
+					}
+				}
+				results += '</p>';
+
 
 				$("#results").html(results);
                         }
