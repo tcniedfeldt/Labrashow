fd00fe821b1add91b8df03e6f122ab38eda03d1e
Thomas Niedfeldt <30270676+tcniedfeldt@users.noreply.github.com>
2018-02-15 20:55:55 -0700

    updated api call

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
-        $("#netflixSubmit").click(function(e) {
-                e.preventDefault();
+        $("#movieSubmit").click(function(e) {
+                //e.preventDefault();
 		console.log("Roulette Begun!\n");
 
-                var myurl= "https://reelgood.com/roulette/netflix?utm_medium=301&utm_source=netflixroulette";
+                var myurl="https://api.themoviedb.org/3/movie/550?api_key=c59173680a889c0419dae6d8320bd867";
                 $.ajax({
                         url : myurl,
                         dataType : "json",
                         success : function(json) {
                                 console.log(json);
 
-                                //var results = "";
+                                var results = "";
                                 //results += '<h2>Answer to Query: ' + value + '</h2>';
                                 //for (var i=0; i < json.items.length; i++) {
                                 //}
-                                //$("#stackResults").html(results);
+                                $("#results").html(results);
                         }
                 });
         });
