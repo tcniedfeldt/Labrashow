ce5533dd761eb2abbff77b268d1d9f4c506311f5
Thomas Niedfeldt <30270676+tcniedfeldt@users.noreply.github.com>
2018-02-15 21:13:34 -0700

    tested json

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
                 e.preventDefault();
 		console.log("Roulette Begun!\n");
 
-                var myurl="https://api.themoviedb.org/3/movie/550?api_key=c59173680a889c0419dae6d8320bd867";
+		var movie = Math.floor(Math.random() * 1000) + 1;
+                var myurl="https://api.themoviedb.org/3/movie/" + movie + "?api_key=c59173680a889c0419dae6d8320bd867";
                 $.ajax({
                         url : myurl,
                         dataType : "json",
